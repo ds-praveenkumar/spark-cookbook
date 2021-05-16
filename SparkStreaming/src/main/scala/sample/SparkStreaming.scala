92c92d2cf38b52ef7207a519d79a5431cd4b48b0
ds-praveenkumar <praveenkr2208@gmail.com>
2021-05-16 22:30:10 +0530

    add streaming wc

--- SparkStreaming/src/main/scala/sample/SparkStreaming.scala
+++ SparkStreaming/src/main/scala/sample/SparkStreaming.scala
@@ -1,6 +1,7 @@
 package sample
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object SparkStreaming {
 
@@ -8,7 +9,22 @@ object SparkStreaming {
     val sc = SparkSession.builder().appName("SparkStreaming").master("local[3]").getOrCreate()
     println(sc.sparkContext.appName)
 
+    // nc -lk 9999
 
+    val df = sc.readStream
+      .format("socket")
+      .option("host","localhost")
+      .option("port",9999)
+      .load()
+
+    val wordsDF = df.select(explode(split(df("value")," ")).alias("word"))
+
+    val count = wordsDF.groupBy("word").count()
+    val query = count.writeStream
+      .format("console")
+      .outputMode("complete")
+      .start()
+      .awaitTermination()
 
   }
 
